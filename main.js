138f9ad09f5d5469b2d4f43830a2e4e2b23ea595
BrunoMaiolo <brunosj@hotmail.it>
2025-12-17 14:55:51 +0100

    Controllare se "i" e' multiplo di 3 e di 5 (oppure se "i" diviso 3 e  diviso 5 ha resto 0)

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ console.log("JS collegato");
 //Per ogni numero da 1 a 100
 //Stampare i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    
+//Controllare se il numero e' multiplo di 3 e di 5 (se "i" diviso 3 ha resto 0 e se "i" diviso 5 ha resto 0)
+const isMultipleOf3 = i & 3 === 0;
+const isMultipleOf5 = i & 5 === 0;
+
 }
