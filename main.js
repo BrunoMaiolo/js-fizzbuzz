aaf5bd609dc923de0ad103b175c9ec72ad524084
BrunoMaiolo <brunosj@hotmail.it>
2025-12-17 15:04:10 +0100

    correzzione della formula (avevo messo & invece di mettere %)

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ console.log("JS collegato");
 for (let i = 1; i <= 100; i++) {
     
 //Controllare se il numero e' multiplo di 3 e di 5 (se "i" diviso 3 ha resto 0 e se "i" diviso 5 ha resto 0)
-const isMultipleOf3 = i & 3 === 0;
-const isMultipleOf5 = i & 5 === 0;
+const isMultipleOf3 = i % 3 === 0;
+const isMultipleOf5 = i % 5 === 0;
+
 
 }
